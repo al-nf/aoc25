2879ac24c02aeef38d2b150a7d6a973568d8e7fd
al-nf <alankjfung@gmail.com>
2025-12-05 23:31:57 -0800

    updated template files

--- day10/part1.cpp
+++ day10/part1.cpp
@@ -3,29 +3,6 @@ using namespace std;
 
 typedef unsigned long long ull;
 
-void solve(const vector<vector<int>>& nums, const vector<char>& ops) {
-    ull sum = 0;
-    
-    for (size_t i = 0; i < nums[0].size(); ++i) {
-        ull res;
-        if (ops[i] == '+') {
-            res = 0;
-            for (size_t j = 0; j < nums.size(); ++j) {
-                res += nums[j][i];
-            }
-            sum += res;
-
-        } else if (ops[i] == '*') {
-            res = 1;
-            for (size_t j = 0; j < nums.size(); ++j) {
-                res *= nums[j][i];
-            }
-            sum += res;
-        }
-    }
-    cout << sum << endl;
-}
-
 int main(int, char *argv[]) 
 {
     auto start = chrono::high_resolution_clock::now();
